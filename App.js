85b565bd77f8d7cdadbe1feda4953e9932eceaa2
idankauf <idankauf@gmail.com>
2022-01-31 22:39:27 +0200

    Without functions

--- App.js
+++ App.js
@@ -2,24 +2,37 @@
 import { StyleSheet, View } from 'react-native';
 import StartPage from './Components/StartPage';
 import LoginPage from './Components/LoginPage';
+import SignUp from './Components/SignUpPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    Montserrat: require('./assets/fonts/MontserratAlternates-Medium.otf'),
+  
+  });
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="StartPage" 
+    <>
+    {loaded && <NavigationContainer style={{fontFamily:'Montserrat',fontSize:30,color:"white"}}>
+      <Stack.Navigator  initialRouteName="SignUp" 
         screenOptions={{
             headerShown: false
         }} 
       >
         <Stack.Screen name="StartPage" component={StartPage} />
         <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+
       </Stack.Navigator>
     </NavigationContainer>
+  }
+  </>  
   );
+
 }
 
 const styles = StyleSheet.create({
