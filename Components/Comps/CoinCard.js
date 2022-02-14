6e631675173dda8a1c3a11cff302f4e0feb33c6e
idankauf <idankauf@gmail.com>
2022-02-14 22:26:23 +0200

    upload pic

---
+++ Components/Comps/CoinCard.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CoinCard = () => {
+  return (
+    <View>
+      <Text>CoinCard</Text>
+    </View>
+  )
+}
+
+export default CoinCard
+
+const styles = StyleSheet.create({
+
+    
+})
