f7ae9c4fecc2c530b9188da5d4d3171f2ec2ca98
Drorlev <80260870+Drorlev@users.noreply.github.com>
2022-04-03 16:28:09 +0300

    Follow Btn

--- Components/Comps/UserDiscoverBio.js
+++ Components/Comps/UserDiscoverBio.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from "react";
 
 const apiUrl = "http://194.90.158.74/bgroup53/test2/tar4/api/Users/?email=";
 
 const UserDiscoverBio = (props) => {
     const [user, setUser] = useState();
+
+    const follow = () =>{
+      alert("Follow me")
+    }
 
     const getUser = () => {
         console.log()
@@ -27,8 +31,15 @@ const UserDiscoverBio = (props) => {
                 
                 if(result != undefined){
                     setUser(<>
-                        <Text style={styles.txt}>{result.Bio}{'\n'}</Text>
-                        <Image source={{uri:result.Image}} style={styles.roundButton1}/>
+                        <View style={styles.leftCol}>
+                          <TouchableOpacity style={styles.followBtn} onPress={follow}>
+                            <Text style={styles.txtFollow}>Follow</Text>
+                          </TouchableOpacity>
+                          <Text style={styles.txt}>{result.Bio}{'\n'}</Text>
+                          </View>
+                        <View style={styles.rightCol}>
+                          <Image source={{uri:result.Image}} style={styles.roundButton1}/>
+                        </View>
                         </>);
                 }
                 
@@ -96,6 +107,34 @@ const styles = StyleSheet.create({
         color:'#fff',
         //alignSelf:'flex-end',
         marginLeft:20,
-        //marginTop:50
+        marginTop:"5%"
+    },
+    rightCol:{
+      //backgroundColor:"blue",
+      width:"40%",
+      height:"100%",
+    },
+    leftCol:{
+      width:"60%",
+      height:"100%",
+     // backgroundColor:"green",
+    },
+    followBtn:{
+      flexDirection:"row",
+      backgroundColor:"white",
+      borderRadius:20,
+      marginTop:"3%",
+      marginLeft:"3%",
+      width:"50%",
+      height:"30%"
+    },
+    txtFollow:{
+      fontSize:20,
+      color:'purple',
+      //alignSelf:'flex-end',
+      marginLeft:"20%",
+      //alignContent:"center",
+      //textAlign:"center"
+      //marginTop:50
     }
 });
