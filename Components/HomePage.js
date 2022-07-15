7e594f9622a70bd39adcadd17caa631e6e67e366
Drorlev <11drorlv@gmail.com>
2022-07-15 15:28:15 +0300

    remove unused

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -29,17 +29,7 @@ const HomePage = ({ route, navigation }) => {
       console.log(e);
     }
   };
-  const backAction = () => {
-    Alert.alert("Hold on!", "Are you sure you want to go back?", [
-      {
-        text: "Cancel",
-        onPress: () => null,
-        style: "cancel"
-      },
-      { text: "YES", onPress: () => BackHandler.exitApp() }
-    ]);
-    return true;
-  };
+  
 
   useEffect(() => {
       AsyncStorage.getItem('loggedInUserEmail').then((token) => {
